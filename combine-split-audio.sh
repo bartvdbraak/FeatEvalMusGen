2934895f553f9fa9fb437c1a0e8aa00607081501
bartvdbraak <bartvdbraak@gmail.com>
2018-07-05 14:31:59 +0200

    bug fixed
    parameters are handled correctly now and converted files are in a seperate folder to avoid mismatching.

--- combine-split-audio.sh
+++ combine-split-audio.sh
@@ -15,14 +15,14 @@ audio_bitrate=${4:-16k}
 audio_sampling_rate=${5:-16000}
 
 cd preprocess/"${dataset_path}"
-
+mkdir conv
 for i in *.mp3 *.ogg *.flac *.wav
 do
-  ffmpeg -i "$i" -ac 1 -ab 16k -ar 16000 "$i.wav"
+  ffmpeg -i "$i" -ac $audio_channels -ab $audio_bitrate -ar $audio_sampling_rate "conv/$i.wav"
 done
 
-converted="temp.wav"
-sox *.wav $converted
+converted="conv/temp.wav"
+sox conv/*.wav $converted
 cd ../../
 mkdir $dataset_path
 length=$(ffprobe -i "preprocess/$dataset_path/$converted" -show_entries format=duration -v quiet -of csv="p=0")
@@ -30,7 +30,8 @@ end=$(echo "$length / $chunk_size - 1" | bc)
 echo "splitting..."
 echo $PWD
 for i in $(seq 0 $end); do
+	pwd
     ffmpeg -hide_banner -loglevel error -ss $(($i * $chunk_size)) -t $chunk_size -i "preprocess/$dataset_path/$converted" "$dataset_path/$i.wav"
 done
 echo "done"
-rm -f "preprocess/$dataset_path/$converted"
+rm -f "preprocess/$dataset_path/conv/$converted"
