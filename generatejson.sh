1603cd932007972452b080719649e593159212ee
bartvdbraak <bartvdbraak@gmail.com>
2018-07-12 04:23:26 +0200

    update generatejson.sh to work more dynamically

--- generatejson.sh
+++ generatejson.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+set -x
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 directory"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <location streaming_extractor_music> <audio-files folder>"
+    echo "Example: $0 ~/Desktop/streaming_extractor_music ~/Desktop/samplernn-pytorch/datasets/preprocess/piano"
     exit
 fi
 
-dataset_path=$1
+extractorcmd=$1
+folder_path=$2
 
-cd "${dataset_path}"
+cd "${folder_path}"
 mkdir json
 
 for i in *.mp3 *.wav
 do
-  essentia_streaming_extractor_music "$i" "json/$i.json"
+  $extractorcmd "$i" "json/$i.json"
 done
